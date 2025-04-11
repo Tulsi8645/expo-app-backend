b968b156d4c4b321d14c03681f23f5e8047b4108
Tulsi <tulsi.gautam0000@gmail.com>
2025-04-11 10:10:55 +0000

    bookworm api update

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import "dotenv/config";
 const app = express();
 const PORT=process.env.PORT || 3001;
 
-import authRoutes from "./routes/authRoutes.js";
-import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 app.use(express.json());
@@ -15,8 +13,6 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.status(200).json({msg: "Bookworm Api is running"})
 })
-app.use("/api/auth", authRoutes);
-app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
